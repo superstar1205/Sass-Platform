5acd2be9d5c37a886bee5c7b47bd4425c74f862b
prodev-web <devking1205@gmail.com>
2022-09-13 20:43:39 +0900

    Create 2022_01_17_082354_create_responses_table.php

---
+++ database/migrations/2022_01_17_082354_create_responses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResponsesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('responses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('form_id');
+            $table->json('form_data');
+            $table->unsignedTinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('responses');
+    }
+}
