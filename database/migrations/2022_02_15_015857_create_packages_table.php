09b7f54ec1da90f7aa33503dec4a0c846fe04751
prodev-web <devking1205@gmail.com>
2022-09-13 20:44:04 +0900

    Create 2022_02_15_015857_create_packages_table.php

---
+++ database/migrations/2022_02_15_015857_create_packages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('forms_number');
+            $table->unsignedInteger('responses_number');
+            $table->unsignedTinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
