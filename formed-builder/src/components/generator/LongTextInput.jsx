02d0929acd8d64fa29549269134d9c974637cdde
prodev-web <devking1205@gmail.com>
2022-09-15 10:48:49 +0900

    Create LongTextInput.jsx

---
+++ formed-builder/src/components/generator/LongTextInput.jsx
@@ -0,0 +1,24 @@
+import BaseInput from './BaseInput'
+
+export default function(props) {
+
+  const { item = {} } = props
+
+  return (
+    <BaseInput
+      item={item}
+      onInputChange={props.onInputChange}
+    >
+      <div className="relative">
+        <textarea
+          style={{'--theme-ring-default': 'transparent'}}
+          value={item.placeholder}
+          onChange={(e) => { props.onInputChange(e.target.value, 'placeholder') }}
+          rows="5"
+          placeholder="Enter a placeholder for this question"
+          className="theme-border theme-ring block w-full px-4 py-3 pr-8 md:pr-4 border rounded transition-colors duration-100 ease-out appearance-none focus:outline-none disabled:bg-gray-50 disabled:cursor-not-allowed text-gray-500 placeholder-gray-500"
+        />
+      </div>
+    </BaseInput>
+  )
+}
