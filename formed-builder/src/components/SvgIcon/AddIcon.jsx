0d8fb41278f3f157bacc4a055450dd9dc33d3ac0
prodev-web <devking1205@gmail.com>
2022-09-20 22:56:38 +0900

    Create AddIcon.jsx
    reward about follow( :) )
    
    Co-Authored-By: HadesHappy <108986787+HadesHappy@users.noreply.github.com>

---
+++ formed-builder/src/components/SvgIcon/AddIcon.jsx
@@ -0,0 +1,7 @@
+export default function({ classList = "" }) {
+  return (
+    <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 24 24" className={classList} height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+      <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
+    </svg>
+  )
+}
