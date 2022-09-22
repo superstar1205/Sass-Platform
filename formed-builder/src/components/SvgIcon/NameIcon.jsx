639a079deb9113b51075e0be96d6010bb39cdff2
prodev-web <devking1205@gmail.com>
2022-09-22 10:14:15 +0900

    Create NameIcon.jsx

---
+++ formed-builder/src/components/SvgIcon/NameIcon.jsx
@@ -0,0 +1,7 @@
+export default function({ classList = "" }) {
+  return (
+    <svg viewBox="0 0 24 24" className={classList}>
+      <path fill="currentColor" d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z"></path>
+    </svg>
+  )
+}
