e09c61bba616708cb37e50eb7e01e760678afe69
prodev-web <devking1205@gmail.com>
2022-09-22 10:09:13 +0900

    Create H1Icon.jsx

---
+++ formed-builder/src/components/SvgIcon/H1Icon.jsx
@@ -0,0 +1,7 @@
+export default function({ classList = "" }) {
+  return (
+    <svg viewBox="0 0 24 24" className={classList}>
+      <path d="M5.29199 17.0004H7.29093V13.0949H11.3488V17.0004H13.3431V7.5459H11.3488V11.4468H7.29093V7.5459H5.29199V17.0004Z" fill="currentColor"></path><path d="M19.0491 7.5459H17.1702L14.8296 9.02779V10.8005L16.9948 9.44327H17.0502V17.0004H19.0491V7.5459Z" fill="currentColor"></path>
+    </svg>
+  )
+}
