04e245fa3b9983bfc4201e806e2362829f07eaa5
prodev-web <devking1205@gmail.com>
2022-09-22 10:13:52 +0900

    Create LongTextIcon.jsx

---
+++ formed-builder/src/components/SvgIcon/LongTextIcon.jsx
@@ -0,0 +1,9 @@
+export default function({ classList = "" }) {
+  return (
+    <svg viewBox="0 0 24 24" className={classList}>
+      <path d="M4 7H12V9H4V7Z" fill="currentColor"></path>
+      <path d="M4 11H20V13H4V11Z" fill="currentColor"></path>
+      <path d="M4 15H20V17H4V15Z" fill="currentColor"></path>
+    </svg>
+  )
+}
