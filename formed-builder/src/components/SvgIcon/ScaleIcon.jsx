1f95f894c1f2cc7471dec05c8dbc789e2d49cb10
prodev-web <devking1205@gmail.com>
2022-09-23 15:33:53 +0900

    Create ScaleIcon.jsx

---
+++ formed-builder/src/components/SvgIcon/ScaleIcon.jsx
@@ -0,0 +1,9 @@
+export default function({ classList = "" }) {
+  return (
+    <svg viewBox="0 0 24 24" className={classList} fill="none" stroke="currentColor">
+      <path d="M2 9.5H6C6.27614 9.5 6.5 9.72386 6.5 10V14C6.5 14.2761 6.27614 14.5 6 14.5H2C1.72386 14.5 1.5 14.2761 1.5 14V10C1.5 9.72386 1.72386 9.5 2 9.5Z" stroke="black"></path>
+      <path d="M10 9.5H14C14.2761 9.5 14.5 9.72386 14.5 10V14C14.5 14.2761 14.2761 14.5 14 14.5H10C9.72386 14.5 9.5 14.2761 9.5 14V10C9.5 9.72386 9.72386 9.5 10 9.5Z" stroke="black"></path>
+      <path d="M18 9.5H22C22.2761 9.5 22.5 9.72386 22.5 10V14C22.5 14.2761 22.2761 14.5 22 14.5H18C17.7239 14.5 17.5 14.2761 17.5 14V10C17.5 9.72386 17.7239 9.5 18 9.5Z" fill="black" stroke="black"></path>
+    </svg>
+  )
+}
