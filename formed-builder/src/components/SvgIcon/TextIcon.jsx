8ab6fe529833156e5d8a214f1b768167810a3959
prodev-web <devking1205@gmail.com>
2022-09-23 15:35:08 +0900

    Create TextIcon.jsx

---
+++ formed-builder/src/components/SvgIcon/TextIcon.jsx
@@ -0,0 +1,7 @@
+export default function({ classList = "" }) {
+  return (
+    <svg viewBox="0 0 24 24" className={classList}>
+      <path d="M10 14V19H12V8H14V19H16V8H18V6H10C8.93913 6 7.92172 6.42143 7.17157 7.17157C6.42143 7.92172 6 8.93913 6 10C6 11.0609 6.42143 12.0783 7.17157 12.8284C7.92172 13.5786 8.93913 14 10 14Z" fill="currentColor"></path>
+    </svg>
+  )
+}
