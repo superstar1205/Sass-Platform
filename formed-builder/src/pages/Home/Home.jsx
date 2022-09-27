d3c3126b78010ac27d432eb0cbed62748db2fff3
prodev-web <devking1205@gmail.com>
2022-09-27 18:19:08 +0900

    Create Home.jsx

---
+++ formed-builder/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import Builder from '../Form/Builder'
+
+export default function() {
+
+  return (
+    <div className="flex bg-white min-h-screen">
+      <Builder />
+      {/* <div className="px-6 bg-white z-50 border-t border-gray-100 flex items-center justify-end md:justify-between py-4 md:px-12">
+        <a
+          href="#"
+          className="rounded focus:ring-2 focus:ring-blue-300 focus:outline-none text-sm font-semibold transition-colors duration-100 ease-out focus:ring disabled:cursor-not-allowed px-8 py-2 bg-white border-2 border-gray-400 text-gray-600 hover:border-gray-600 hover:text-gray-800 mr-4">
+          Preview
+        </a>
+      </div> */}
+    </div>
+  )
+}
