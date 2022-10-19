0ccf4af507fd7faa2f7cf4962d773f0dbf386446
prodev-web <devking1205@gmail.com>
2022-10-19 23:43:20 +0900

    Create pricing.blade.php

---
+++ resources/views/mockup/pricing.blade.php
@@ -0,0 +1,145 @@
+<x-guest-layout>
+
+
+    <div class="max-w-5xl mx-auto mt-10 space-y-4">
+
+        <div class="w-full mx-auto px-4 flex items-center justify-center flex-col">
+            <h1 class="font-semibold text-gray-900 text-5xl">Pricing</h1>
+            <p class="text-lg py-4">
+                We've got a plan for any size
+            </p>
+        </div>
+
+
+        <div class="grid grid-cols-3 gap-4">
+
+            <div
+                class="rounded-md border border-gray-300 px-4 py-10 flex items-center justify-center flex-col space-y-6">
+
+                <div>
+                    <h2 class="text-4xl font-bold">Starter</h2>
+                </div>
+                <div>
+                    <p class="text-4xl font-bold">$5/<span class="text-base">month</span></p>
+                </div>
+
+                <div>
+                    <ul class="flex flex-col space-y-2">
+                        <li class="inline-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M5 13l4 4L19 7"/>
+                            </svg>
+
+                            1 forms
+                        </li>
+
+                        <li class="inline-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M5 13l4 4L19 7"/>
+                            </svg>
+                            1000 responses
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div class="w-full">
+                    <a href="#" class="py-4 w-full flex items-center justify-center border hover:bg-gray-50">
+                        Select
+                    </a>
+                </div>
+            </div>
+
+            <div
+                class="rounded-md border border-gray-300 px-4 py-10 flex items-center justify-center flex-col space-y-6">
+
+                <div>
+                    <h2 class="text-4xl font-bold">Advanced</h2>
+                </div>
+                <div>
+                    <p class="text-4xl font-bold">$20/<span class="text-base">month</span></p>
+                </div>
+
+                <div>
+                    <ul class="flex flex-col space-y-2">
+                        <li class="inline-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M5 13l4 4L19 7"/>
+                            </svg>
+
+                            2 forms
+                        </li>
+
+                        <li class="inline-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M5 13l4 4L19 7"/>
+                            </svg>
+                            2000 responses
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div class="w-full">
+                    <a href="#" class="py-4 w-full flex items-center justify-center border hover:bg-gray-50">
+                        Select
+                    </a>
+                </div>
+            </div>
+
+            <div
+                class="rounded-md border border-gray-300 px-4 py-10 flex items-center justify-center flex-col space-y-6">
+
+                <div>
+                    <h2 class="text-4xl font-bold">Best</h2>
+                </div>
+                <div>
+                    <p class="text-4xl font-bold">$30/<span class="text-base">month</span></p>
+                </div>
+
+                <div>
+                    <ul class="flex flex-col space-y-2">
+                        <li class="inline-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M5 13l4 4L19 7"/>
+                            </svg>
+
+                            3 forms
+                        </li>
+
+                        <li class="inline-flex">
+                            <svg xmlns="http://www.w3.org/2000/svg"
+                                 class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M5 13l4 4L19 7"/>
+                            </svg>
+                            3000 responses
+                        </li>
+                    </ul>
+
+                </div>
+
+                <div class="w-full">
+                    <a href="#" class="py-4 w-full flex items-center justify-center border hover:bg-gray-50">
+                        Select
+                    </a>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+
+
+</x-guest-layout>
